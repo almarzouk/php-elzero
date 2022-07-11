67bfd648b11c717e6e87f8a582dddddb08ff977c
jumaamrz <jmoooo32@gmail.com>
2022-07-11 14:47:34 +0200

    Introduction

--- week_01/001.php
+++ week_01/001.php
@@ -1,1 +1,37 @@
 <?php
+
+/* what is PHP ?*/
+
+// [1] Scripting language suited for web development
+// [2] Used in back-end " script are excuted on the server"
+// [3] Free
+// [4] Open source
+
+/* websites that use PHP */
+
+// old version of facebook
+// wordpress
+// Most CMS
+// To search Famus websites and companies that use PHP in 2022
+
+/* Why PHP */
+// Easy to install and learn
+// BIg community for support
+// Famous framework "Laravel"
+// Work with Many Databases
+
+/* what can you do with PHP */
+
+// Create Dynamic Pages
+// Create, open, read, write and delete files on the server
+// Encrypt and decrypt data ******
+// Deal with databases
+// Deal with Images and files
+// Deal with browser cookies
+
+/* what you will learn in the course PHP */
+// PHP Core anguage
+// PHP OOP
+// PHP MVC Pattern
+
+/* what you Need To Learn */
