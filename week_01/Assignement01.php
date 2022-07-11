4975f1a406351ea2adeb7d491c6a8a60866d3ce7
jumaamrz <jmoooo32@gmail.com>
2022-07-11 16:14:36 +0200

    Assignement for the 1st week

---
+++ week_01/Assignement01.php
@@ -0,0 +1,62 @@
+<?php
+/*
+====================
+== Assignement -1 ==
+====================
+*/
+
+
+/*
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="<?= "UTF-8" ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+</head>
+
+<body>
+    <div><?= "We love php" ?></div>
+    <div><?= "Elzero channel" ?></div>
+</body>
+
+</html>
+
+*/
+/*
+====================
+== Assignement -2 ==
+====================
+*/
+
+
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+
+
+/*
+====================
+== Assignement -3 ==
+====================
+*/
+
+
+/*
+My Application
+Version 1.0
+Created By Elzero
+*/
+
+/*
+====================
+== Assignement -4 ==
+====================
+*/
+
+# First Comment
+#//Second Comment
+/* Third Comment */
+//Fourth Comment
