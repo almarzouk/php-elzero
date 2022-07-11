94766eb044bc36fb749be7bdc1e8e0af2f3ce13d
jumaamrz <jmoooo32@gmail.com>
2022-07-11 16:01:21 +0200

    Comments And Best Practices

---
+++ week_01/005.php
@@ -0,0 +1,22 @@
+<?php
+//  Comments And Best Practices
+// Single line comment
+
+echo "hi"; // Single line comment
+
+/* 
+Line 1
+Line 2
+*/
+
+/*
+====================
+====== my App ======
+====================
+*/
+/* Single line  */
+
+echo "hi"; /* Single line  */
+
+// I used Print because it's faster than Echo 
+print 'test';
