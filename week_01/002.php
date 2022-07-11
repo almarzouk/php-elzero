d38069059c81db876b10e14a52ecc7df2829fd0d
jumaamrz <jmoooo32@gmail.com>
2022-07-11 14:54:48 +0200

    what is new and how to study the course

--- week_01/002.php
+++ week_01/002.php
@@ -1,1 +1,10 @@
 <?php
+/* What is new */
+
+// last version
+// Less time "Notes and articles on website"
+// Concepts will be explained
+// Video and audio quality + cam
+// Assignement and challenges
+// study plan
+// Prepare for FrameWorks
