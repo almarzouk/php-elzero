697ea8d7f2da1ccb7b377985e886e2d8a54be340
jumaamrz <jmoooo32@gmail.com>
2022-07-12 11:21:52 +0200

    week 2 lesson 9

---
+++ week_02/009.php
@@ -0,0 +1,31 @@
+<?php
+/*
+================
+# Data types #
+================
+Boolean And Converting To Boolean
+=========================================
+*/
+
+var_dump((bool) '');
+echo '</br>';
+var_dump((bool) []);
+echo '</br>';
+var_dump((bool) array());
+echo '</br>';
+var_dump((bool) 0);
+echo '</br>';
+var_dump((bool) "0");
+echo '</br>';
+var_dump((bool) [0]);
+echo '</br>';
+var_dump((bool) 1);
+echo '</br>';
+var_dump((bool) "1");
+echo '</br>';
+var_dump((bool) -1);
+echo '</br>';
+var_dump((bool) 1.5);
+echo '</br>';
+var_dump((bool) -1.5);
+echo '</br>';
