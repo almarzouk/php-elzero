b244a1815597e86049ac101b8567f028f2b3df39
jumaamrz <jmoooo32@gmail.com>
2022-07-12 11:31:55 +0200

    week 2 lesson 10

---
+++ week_02/010.php
@@ -0,0 +1,31 @@
+<?php
+/*
+================
+# Data types #
+================
+ String And Escaping
+=========================================
+*/
+
+echo 'Hello PHP';
+echo '</br>';
+echo "Hello PHP";
+echo '</br>';
+echo "Hello 'PHP'";
+echo '</br>';
+echo 'Hello "PHP"';
+echo '</br>';
+echo 'Hello \'PHP\'';
+echo '</br>';
+echo 'Hello PHP \\';
+echo '</br>';
+echo 'Hello PHP 
+Multiple 
+Lines
+';
+echo '</br>';
+echo '</br>';
+echo nl2br('Hello PHP 
+Multiple 
+Lines
+');
