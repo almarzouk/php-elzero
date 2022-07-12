81366bf851199d971706c2674e69b234d06b7a64
jumaamrz <jmoooo32@gmail.com>
2022-07-12 11:55:36 +0200

    week 2 lesson 12

---
+++ week_02/012.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+  ============================================
+  = Data Types
+  = ----------
+  = Array
+  = -------------------
+  = Array With Key
+  = Array Without Keys
+  = Array With One Key
+  = Array Value Override
+  = ------------------
+  = print_r()
+  ============================================
+  */
+
+echo '<pre>';
+print_r([
+    0 => "Sameh",
+    "A" => "Ahmed",
+    "B" => "Basem",
+    "Mahmoud",
+    True => "Sayed",
+    "1" => "Osama",
+    "Gamal",
+    9 => "Amera",
+    "Eman",
+    "Mohamed",
+    False => "Asmaa",
+    8 => "Haytham",
+    "Samer",
+    "Names" => [
+        "Osama",
+        "Ahmed",
+        "Sayed" => [
+            "1",
+            "2",
+            "3"
+        ]
+    ]
+]);
+echo '</pre>';
