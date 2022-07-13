b2fbc4c53e90d6edc08d728843855f34d122bb6a
jumaamrz <jmoooo32@gmail.com>
2022-07-13 14:23:44 +0200

    week 3 assignment 3

---
+++ week_03/Assignement03.php
@@ -0,0 +1,56 @@
+<!-- Assignement 1 -->
+<?php
+$elzero = "jumaa Courses";
+?>
+<!-- 
+
+    <!DOCTYPE html>
+    <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content=<?= $elzero ?>>
+            <title>Welcome To <?= $elzero ?></title>
+        </head>
+        
+        <body>
+            <h1><?= $elzero ?></h1>
+            <p>Here In <?= $elzero ?> We Provide Front-End And Back-End Courses</p>
+            <hr>
+            <div><?= $elzero ?> Is The What You Need.</div>
+            <footer>All Right Reserved To <?= $elzero ?></footer>
+        </body>
+        
+        </html>
+        
+        
+    -->
+
+
+<!-- Assignement 2 -->
+
+<?php
+$name = "elzero";
+$$name = "Web";
+
+echo $$name;
+echo '<br>';
+echo $$name;
+echo '<br>';
+echo $$name;
+echo '<br>';
+echo $$name;
+echo '<br>';
+echo $$name;
+echo '<br>';
+?>
+<!-- Assignement 3 -->
+<?php
+$a = 200;
+$b = &$a;
+$a = 100;
+
+echo $b; // 100
+?>
