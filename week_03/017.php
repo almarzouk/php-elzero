4b9344663e24acb9111d1118f212668ef1767b3a
jumaamrz <jmoooo32@gmail.com>
2022-07-13 14:03:33 +0200

    week 3 lesson 17

---
+++ week_03/017.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+    Pre-Defined Variables
+
+    Search
+    - PHP Pre-Defined Variables
+  */
+
+// echo '<pre>';
+// print_r($_SERVER);
+// echo $_SERVER["HTTP_CONNECTION"];
+// echo '</pre>';
+
+echo $_GET["username"];
+
+?>
+
+<form action="" method="get">
+    <input type="text" name="username">
+    <input type="submit" value="Send">
+</form>
