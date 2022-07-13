c4ee6853e05098246d88b28d1d8a8ebc3a07ae33
jumaamrz <jmoooo32@gmail.com>
2022-07-13 10:08:46 +0200

    week 3 lesson 13

--- week_03/013.php
+++ week_03/013.php
@@ -1,1 +1,26 @@
 <?php
+
+/*
+    Variables
+    - Naming Rules + Info
+    [1] Start With Dollar Sign $
+    [2] Start With A Letter [a-z] Or [A-Z] Or Underscore
+    [3] You Can Use Numbers Inside The Name
+    [4] No Special Characters Allowed
+    [5] Case Sensitive
+    - Test Single And Double Quotes
+
+    Search
+    - Loosely Typed Language
+  */
+
+$username = "Jumaa";
+$Username = "ALMARZOUK";
+
+echo $username;
+echo '<br>';
+echo $Username;
+echo '<br>';
+echo 'Hello $username';
+echo '<br>';
+echo "Hello $username";
