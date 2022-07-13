8f1db1381037e63f1a4d3615d08f1bd68b23e574
jumaamrz <jmoooo32@gmail.com>
2022-07-13 10:19:10 +0200

    week 3 lesson 14

--- week_03/014.php
+++ week_03/014.php
@@ -1,1 +1,17 @@
-<?php
+<?php $username = "Jumaa" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Page | <?php echo $username ?></title>
+</head>
+
+<body>
+    <div>Welcome <?php echo $username ?></div>
+    <div><?php echo $username ?> You Scored 1000 Points</div>
+</body>
+
+</html>
