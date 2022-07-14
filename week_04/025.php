a1b5c83f7a4bd76c5a6cef1d641ddf3d3d2e101b
almarzouk <jumaa.almarzouk@.com>
2022-07-14 15:36:42 +0200

    week 4 lesson 25

--- week_04/025.php
+++ week_04/025.php
@@ -1,1 +1,21 @@
 <?php
+// logical operator 
+
+/*
+
+and => and => two are true
+&&  => and => two are true
+or  => and => one or both are true
+||  => and => one or both are true
+xor => xor => one Is True but not both
+!   => Not true 
+*/
+
+var_dump(100 > 50 and 100 > 80 and 100 > 90);
+echo '<br>';
+var_dump(100 > 50 && 100 > 80 && 100 > 90);
+echo '<br>';
+var_dump(100 > 50 or 110 > 120 or 110 > 90);
+echo '<br>';
+var_dump(100 > 50 xor 80 > 90);
+echo '<br>';
