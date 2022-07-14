878de7b4056ef323ee5732dc07290e9ef34744db
almarzouk <jumaa.almarzouk@.com>
2022-07-14 14:35:16 +0200

    week 4 lesson  23

---
+++ week_04/023.php
@@ -0,0 +1,33 @@
+<?php
+/*
+//  Comparison Operators Part 2
+
+    >      => larger than
+    >=     => larger than or equal
+    >      => smaller than
+    >=     => smaller than or equal
+    <=>    => Spaceship [less thanm equal or greater]
+
+*/
+
+// Test equal
+var_dump(100 > 50);
+echo '<br>';
+var_dump(100 >= 50);
+echo '<br>';
+var_dump(100 < 50);
+echo '<br>';
+var_dump(100 <= 50);
+echo '<br>';
+var_dump(100 <=> 110);
+echo '<br>';
+var_dump(100 <=> 100);
+echo '<br>';
+var_dump(100 <=> 50);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
