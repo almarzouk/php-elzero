6868f6af631d0fc08c6a40738e346c13b3303e1d
almarzouk <jumaa.almarzouk@.com>
2022-07-17 14:15:28 +0200

    week 5 Assignments part 2 done

---
+++ week_05/Assignement05-02.php
@@ -0,0 +1,71 @@
+<?php
+// $index = 10;
+// for (; $index > 0; $index--) {
+//     echo $index . '<br>';
+// }
+
+// $index = 0;
+// for (; $index <= 20; $index++) {
+//     if ($index % 2 == 0) {
+//         if ($index === 0) {
+//             continue;
+//         }
+//         echo $index . '<br>';
+//     }
+// }
+
+// $num = 2;
+// while ($num < 520) {
+//     if ($num == 2) {
+//         echo $num / $num . '<br>';
+//     }
+//     echo $num + 2 . '<br>';
+//     $num = ($num + 2) * 2;
+// }
+// $start = 10;
+// $end = 0;
+// $stop = 3;
+
+// for ($i = $start; $i >=  $stop; $i--) {
+//     if ($i < $start) {
+//         echo "0$i<br>";
+//     } else {
+//         echo "$i<br>";
+//     }
+// }
+
+// $start = 0;
+// $mix = [1, 2, 3, "A", "B", "C", 4];
+
+// for ($i = 0; $i <= count($mix); $i++) {
+//     if (gettype($mix[$i]) === "integer") {
+//         echo $mix[$i];
+//         continue;
+//     }
+// }
+
+// $money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+
+// // for ($i = 0; $i <= count($money); $i++) {
+// //     echo $money[$i];
+// // }
+// foreach ($money as $key => $val) {
+//     echo "The Name Is $key And I Need $val Pound From Him <br>";
+// }
+
+// $mix = [1, 2, "A", "B", "C", 3, 4];
+
+// foreach ($mix as $m) {
+//     if (gettype($m) === 'integer') {
+//         echo $m . '<br>';
+//         echo '<br>';
+//         continue;
+//     }
+// }
+
+// $nums = [1, 13, 12, 20, 51, 17, 30];
+// foreach ($nums as $num) {
+//     if ($num % 2 === 0) {
+//         echo $num / 2 . '<br>';
+//     }
+// }
