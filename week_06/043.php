f1b4229898026fdba2a68ba29c1c7a6f8130c52e
jumaamrz <jmoooo32@gmail.com>
2022-07-18 09:46:04 +0200

    week 6 lesson 43

---
+++ week_06/043.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+FUNCTION
+    -Introduction And DRY Concept
+    -Parameter
+    -Argument
+
+*/
+
+function sayHello($name)
+{
+    echo "Hello " . $name . '<br>';
+}
+
+sayHello('jumaa');
+sayHello('rama');
+sayHello('sara');
+sayHello('nour');
